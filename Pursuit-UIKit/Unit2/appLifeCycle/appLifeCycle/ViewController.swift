580183af81f1f58a9e61db6ebdd9a563d3c5b6fc
chakane <cs499@njit.edu>
2021-12-05 20:56:39 -0500

    init

---
+++ Pursuit-UIKit/Unit2/appLifeCycle/appLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appLifeCycle
+//
+//  Created by Chakane Shegog on 12/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
