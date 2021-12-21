b7a3372cff3cb1d08bb6cd3a89042edf41e92b67
chakane <cs499@njit.edu>
2021-12-21 13:47:30 -0500

    init getR

---
+++ Pursuit-UIKit/Unit2/getR/getR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  getR
+//
+//  Created by Chakane Shegog on 12/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
