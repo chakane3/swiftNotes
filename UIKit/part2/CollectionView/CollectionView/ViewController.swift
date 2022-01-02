49c0053d6a70e656879250ef98a368d00fa7b2f8
chakane3 <39193557+chakane3@users.noreply.github.com>
2022-01-02 01:35:03 -0500

    init

---
+++ UIKit/part2/CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Chakane Shegog on 1/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
