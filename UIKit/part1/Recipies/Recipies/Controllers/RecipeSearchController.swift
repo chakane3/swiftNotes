c1b8f11ed5cd17667981084d4b2cfbbac1e04443
chakane3 <39193557+chakane3@users.noreply.github.com>
2022-01-03 21:53:53 -0500

    fin

--- UIKit/part1/Recipies/Recipies/Controllers/RecipeSearchController.swift
+++ UIKit/part1/Recipies/Recipies/Controllers/RecipeSearchController.swift
@@ -8,10 +8,78 @@
 import UIKit
 
 class RecipeSearchController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    // recipes array
+    var recipes = [Recipe]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        searchRecipes(searchQuery: "Beef")
+    }
+    
+    // this function takes the users query and performs our network request
+    func searchRecipes(searchQuery: String) {
+        RecipeAPI.fetchRecipe(for: searchQuery, completion: {[weak self] (result) in
+            switch result {
+            case .failure(let appError):
+                print("error: \(appError)")
+            case .success(let recipes):
+                self?.recipes = recipes
+            }
+        })
+    }
+}
+
+
+// MARK: - TableView datasource and delegate methods
+extension RecipeSearchController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "recipeCell", for: indexPath) as? RecipeCell else {
+            fatalError("could not dequeue recipe cell")
+        }
+        
+        let recipe = recipes[indexPath.row]
+        cell.configureCell(for: recipe)
+        return cell
+    }
+}
+
+extension RecipeSearchController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
+}
+
+
+// MARK: - Searchbar datasource and delegate methods
+extension RecipeSearchController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        // dismiss they keyboard when the user clicks the search button
+        searchBar.resignFirstResponder()
+        
+        // we need to unwrap the searchBar.text property because its an optional
+        guard let searchText = searchBar.text else {
+            print("missing search text")
+            return
+        }
+        searchRecipes(searchQuery: searchText)
     }
 }
 
