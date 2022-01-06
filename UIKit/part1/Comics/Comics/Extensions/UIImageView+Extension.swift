95a3723f021962c998024e1ef102e9f9bc49eafb
chakane3 <39193557+chakane3@users.noreply.github.com>
2022-01-06 03:14:39 -0500

    init

--- UIKit/part1/Comics/Comics/Extensions/UIImageView+Extension.swift
+++ UIKit/part1/Comics/Comics/Extensions/UIImageView+Extension.swift
@@ -14,11 +14,11 @@ extension UIImageView {
     public func getImage(with urlString: String, completion: @escaping (Result<UIImage, AppError>) -> ()) {
         
         // implement a UIActivityIndicatorView to tell the user a donwload is in progress
-//        let activityIndicator = UIActivityIndicatorView(style: .large)
-//        activityIndicator.color = UIColor.systemOrange
-//        activityIndicator.startAnimating()
-//        addSubview(activityIndicator)
-//        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.color = UIColor.systemOrange
+        activityIndicator.startAnimating()
+        addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
 //        NSLayoutConstraint.activate([activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor), activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)])
         
         guard let url = URL(string: urlString) else {
