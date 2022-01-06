af37d5c5f64e6b70c545ea4cc61358d01501d025
chakane3 <39193557+chakane3@users.noreply.github.com>
2022-01-05 19:24:43 -0500

    initial commit

---
+++ UIKit/part1/RandomUsers/RandomUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUsers
+//
+//  Created by Chakane Shegog on 1/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
