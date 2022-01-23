2bb41c642b34d6ab5f7d21ccb062c99fa281124a
chakane3 <39193557+chakane3@users.noreply.github.com>
2022-01-23 18:40:28 -0500

    setup data persistence

---
+++ UIKit/part2/NYT/NYT/DataPersistence.swift
@@ -0,0 +1,123 @@
+//
+//  DataPersistence.swift
+//  NYT
+//
+//  Created by Chakane Shegog on 1/23/22.
+//
+
+import Foundation
+
+public enum DataPersistenceError: Error {
+    case propertyListEncodingError(Error)
+    case propertyListDecodingError(Error)
+    case writingError(Error)
+    case deletingError
+    case noContentsAtPath(String)
+}
+
+public protocol DataPersistenceDelegate: AnyObject {
+    func didSaveItem<T: Codable>(_ persistenceHelper: DataPersistence<T>, item: T)
+    func didDeleteItem<T: Codable>(_ persistenceHelper: DataPersistence<T>, item: T)
+}
+
+public typealias Writeable = Codable & Equatable
+
+public final class DataPersistence<T: Writeable> {
+    private let filename: String
+    private var items: [T]
+    public weak var delegate: DataPersistenceDelegate?
+    
+    public init(filename: String) {
+        self.filename = filename
+        self.items = []
+    }
+    
+    private func saveItemsToDocumentsDirectory() throws {
+        do {
+            let url = FileManager.getPath(with: filename, for: .documentsDirectory)
+            let data = try PropertyListEncoder().encode(items)
+            try data.write(to: url, options: .atomic)
+        } catch {
+            throw DataPersistenceError.writingError(error)
+        }
+    }
+    
+    public func createItem(_ item: T) throws {
+        _ = try? loadItems()
+        items.append(item)
+        do {
+            try saveItemsToDocumentsDirectory()
+            delegate?.didSaveItem(self, item: item)
+        } catch {
+            throw DataPersistenceError.writingError(error)
+        }
+    }
+    
+    public func loadItems() throws -> [T] {
+        let path = FileManager.getPath(with: filename, for: .documentsDirectory).path
+        if FileManager.default.fileExists(atPath: path) {
+            if let data = FileManager.default.contents(atPath: path) {
+                do {
+                    items = try PropertyListDecoder().decode([T].self, from: data)
+                } catch {
+                    throw DataPersistenceError.propertyListDecodingError(error)
+                }
+            }
+        }
+        return items
+    }
+    
+    // reordering
+    public func synchronize(_ items: [T]) {
+        self.items = items
+        try? saveItemsToDocumentsDirectory()
+    }
+    
+    @discardableResult
+    public func update(_ item: T, at index: Int) -> Bool {
+        items[index] = item
+        try? saveItemsToDocumentsDirectory()
+        return true
+    }
+    
+    @discardableResult
+    public func update(_ oldItem: T, with newItem: T) -> Bool {
+        if let index = items.firstIndex(of: oldItem) {
+            update(newItem, at: index)
+            return true
+        }
+        return false
+    }
+    
+    public func deleteItem(at index: Int) throws {
+        let deletedItem = items[index]
+        items.remove(at: index)
+        do {
+            try saveItemsToDocumentsDirectory()
+            delegate?.didDeleteItem(self, item: deletedItem)
+        } catch {
+            throw DataPersistenceError.deletingError
+        }
+    }
+    
+    public func hasItemBeenSaved(_ item: T) -> Bool {
+        guard let items = try? loadItems() else {
+            return false
+        }
+        
+        self.items = items
+        if let _ = self.items.firstIndex(of: item) {
+            return true
+        }
+        return false
+    }
+    
+    public func removeAll() {
+        guard let loadedItems = try? loadItems() else {
+            return
+        }
+        items = loadedItems
+        items.removeAll()
+        try? saveItemsToDocumentsDirectory()
+    }
+}
---
+++ UIKit/part2/NYT/NYT/UIImage+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+Extensions.swift
+//  NYT
+//
+//  Created by Chakane Shegog on 1/23/22.
+//
+
+import Foundation
