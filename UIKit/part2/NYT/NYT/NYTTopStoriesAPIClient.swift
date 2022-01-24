6a1d51ff94f0beee464c767006e990dc980b08ff
chakane3 <39193557+chakane3@users.noreply.github.com>
2022-01-24 09:25:05 -0500

    Update NYTTopStoriesAPIClient.swift

--- UIKit/part2/NYT/NYT/NYTTopStoriesAPIClient.swift
+++ UIKit/part2/NYT/NYT/NYTTopStoriesAPIClient.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct NYTTopStoriesAPIClient {
+    
+    static func fetchTopStories(for section: String, completion: @escaping (Result<[Article], NetworkError>) -> ()) {
+        let endpointURLString = "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key="
+        guard let url = URL(string: endpointURLString) else {
+            completion(.failure(.badURL(endpointURLString)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkRequest.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let networkError):
+                completion(.failure(.networkClientError(networkError)))
+            case .success(let data):
+                do {
+                    let topStories = try JSONDecoder().decode(TopStories.self, from: data)
+                    completion(.success(topStories.results))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
